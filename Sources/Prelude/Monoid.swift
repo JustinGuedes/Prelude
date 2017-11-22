fb543b4b5ae8cb0ba92caace3841089770c83f3a
JustinGuedes <justin.guedes@gmail.com>
2017-11-22 19:42:55 +0200

    Update Monoids and Semigroups and remove useless tests

--- Sources/Prelude/Monoid.swift
+++ Sources/Prelude/Monoid.swift
@@ -10,3 +10,119 @@ public protocol Monoid: Semigroup {
     static var empty: Self { get }
     
 }
+
+// MARK: - Standard Monoids
+
+extension Array: Monoid {
+    
+    public static var empty: Array {
+        return []
+    }
+    
+}
+
+extension String: Monoid {
+    
+    public static var empty: String {
+        return ""
+    }
+    
+}
+
+// MARK: - Number Type Monoids
+
+extension Int: Monoid {
+    
+    public static var empty: Int {
+        return 0
+    }
+    
+}
+
+extension Int8: Monoid {
+    
+    public static var empty: Int8 {
+        return 0
+    }
+    
+}
+
+extension Int16: Monoid {
+    
+    public static var empty: Int16 {
+        return 0
+    }
+    
+}
+
+extension Int32: Monoid {
+    
+    public static var empty: Int32 {
+        return 0
+    }
+    
+}
+
+extension Int64: Monoid {
+    
+    public static var empty: Int64 {
+        return 0
+    }
+    
+}
+
+extension UInt: Monoid {
+    
+    public static var empty: UInt {
+        return 0
+    }
+    
+}
+
+extension UInt8: Monoid {
+    
+    public static var empty: UInt8 {
+        return 0
+    }
+    
+}
+
+extension UInt16: Monoid {
+    
+    public static var empty: UInt16 {
+        return 0
+    }
+    
+}
+
+extension UInt32: Monoid {
+    
+    public static var empty: UInt32 {
+        return 0
+    }
+    
+}
+
+extension UInt64: Monoid {
+    
+    public static var empty: UInt64 {
+        return 0
+    }
+    
+}
+
+extension Float: Monoid {
+    
+    public static var empty: Float {
+        return 0.0
+    }
+    
+}
+
+extension Double: Monoid {
+    
+    public static var empty: Double {
+        return 0.0
+    }
+    
+}
--- Sources/Prelude/Semigroup.swift
+++ Sources/Prelude/Semigroup.swift
@@ -9,6 +9,7 @@
 
 precedencegroup SemigroupPrecedence {
     associativity: left
+    higherThan: AssignmentPrecedence
 }
 
 // MARK: - Operators
@@ -20,5 +21,121 @@ infix operator <> : SemigroupPrecedence
 public protocol Semigroup {
     
     static func <> (lhs: Self, rhs: Self) -> Self
+    
+}
+
+// MARK: - Standard Semigroups
+
+extension Array: Semigroup {
+    
+    public static func <> (lhs: Array, rhs: Array) -> Array {
+        return lhs + rhs
+    }
+    
+}
+
+extension String: Semigroup {
+    
+    public static func <> (lhs: String, rhs: String) -> String {
+        return lhs + rhs
+    }
+    
+}
+
+// MARK: - Number Type Semigroups
+
+extension Int: Semigroup {
+    
+    public static func <> (lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+}
+
+extension Int8: Semigroup {
+    
+    public static func <> (lhs: Int8, rhs: Int8) -> Int8 {
+        return lhs + rhs
+    }
+    
+}
+
+extension Int16: Semigroup {
+    
+    public static func <> (lhs: Int16, rhs: Int16) -> Int16 {
+        return lhs + rhs
+    }
+    
+}
+
+extension Int32: Semigroup {
+    
+    public static func <> (lhs: Int32, rhs: Int32) -> Int32 {
+        return lhs + rhs
+    }
+    
+}
+
+extension Int64: Semigroup {
+    
+    public static func <> (lhs: Int64, rhs: Int64) -> Int64 {
+        return lhs + rhs
+    }
+    
+}
+
+extension UInt: Semigroup {
+    
+    public static func <> (lhs: UInt, rhs: UInt) -> UInt {
+        return lhs + rhs
+    }
+    
+}
+
+extension UInt8: Semigroup {
+    
+    public static func <> (lhs: UInt8, rhs: UInt8) -> UInt8 {
+        return lhs + rhs
+    }
+    
+}
+
+extension UInt16: Semigroup {
+    
+    public static func <> (lhs: UInt16, rhs: UInt16) -> UInt16 {
+        return lhs + rhs
+    }
+    
+}
+
+extension UInt32: Semigroup {
+    
+    public static func <> (lhs: UInt32, rhs: UInt32) -> UInt32 {
+        return lhs + rhs
+    }
+    
+}
+
+extension UInt64: Semigroup {
+    
+    public static func <> (lhs: UInt64, rhs: UInt64) -> UInt64 {
+        return lhs + rhs
+    }
+    
+}
+
+extension Float: Semigroup {
+    
+    public static func <> (lhs: Float, rhs: Float) -> Float {
+        return lhs + rhs
+    }
+    
+}
+
+extension Double: Semigroup {
+    
+    public static func <> (lhs: Double, rhs: Double) -> Double {
+        return lhs + rhs
+    }
     
 }
